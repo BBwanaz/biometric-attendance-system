bbc9d5f6ddebb3aa5dd77286fd1df967e6b94237
Brandon Bwanakocha <38164088+BBwanaz@users.noreply.github.com>
2020-09-08 20:51:29 -0700

    Update text.py

--- text.py
+++ text.py
@@ -6,5 +6,5 @@ client = nexmo.Client(key='f7cc0105', secret='skbMFLX1yz0rGVZc')
 client.send_message({
     'from': '12262770917',
     'to': '17789980302',
-    'text': 'Fuck you python',
+    'text': 'Hi you have signed in',
 })
